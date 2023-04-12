dfea5cd7a40286173d0a39b4771169ed1fbd33e0
DemonKillerh <40468574+Fazil29@users.noreply.github.com>
2023-04-12 23:10:10 +0530

    added NGER.java

---
+++ NGER.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class NGER{
+    
+    public static int[] NextGreaterFromRight(int arr[]) {
+        Stack<Integer> stack = new Stack<>();
+        int ans[] = new int[arr.length];
+
+        for(int i=arr.length-1; i>=0;i--){
+            while(!stack.empty() && stack.peek() <= arr[i]){
+                stack.pop();
+            }
+            if(stack.empty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i]= stack.peek();
+            }
+            stack.push(i);
+        }
+        return ans; 
+    }
+  
+    public static void main(String[] args) {
+        int arr[]= {2,1,5,6,2,3};
+        int ans[] = NextGreaterFromRight(arr);
+        for(int a: ans){
+                System.out.print(a + " ");
+        }
+    }
+}
